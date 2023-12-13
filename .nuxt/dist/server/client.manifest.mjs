8ab9a8160423c66c26270e1d58818772e38eca0e
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2023-12-13 16:45:31 +0545

    Initial commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/BS/Desktop/New%20folder/tiktok-clone/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <TopNav />
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import TopNav from '../components/TopNav.vue';
+
+</script>
