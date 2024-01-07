27c3aca49497c361a8af73a67a25e63d3f78bb98
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2024-01-07 16:19:44 +0545

    like unlike

--- stores/user.js
+++ stores/user.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
-import axios from "~/plugins/axios";
+import axios from "../plugins/axios";
+import { useGeneralStore } from "./general";
 
 const $axios = axios().provide.axios;
 
@@ -15,7 +16,14 @@ export const useUserStore = defineStore("user", {
       await $axios.get("/sanctum/csrf-cookie");
     },
 
-    async register(email, password, confirmPassword) {
+    async login(email, password) {
+      await $axios.post("/login", {
+        email: email,
+        password: password,
+      });
+    },
+
+    async register(name, email, password, confirmPassword) {
       await $axios.post("/register", {
         name: name,
         email: email,
@@ -24,11 +32,13 @@ export const useUserStore = defineStore("user", {
       });
     },
 
-    async login(email, password) {
-      await $axios.post("/login", {
-        email: email,
-        password: password,
-      });
+    async getUser() {
+      let res = await $axios.get("/api/logged-in-user");
+
+      this.$state.id = res.data[0].id;
+      this.$state.name = res.data[0].name;
+      this.$state.bio = res.data[0].bio;
+      this.$state.image = res.data[0].image;
     },
 
     async updateUserImage(data) {
@@ -46,6 +56,87 @@ export const useUserStore = defineStore("user", {
       return await $axios.post("/api/posts", data);
     },
 
+    async deletePost(post) {
+      return await $axios.delete(`/api/posts/${post.id}`);
+    },
+
+    async addComment(post, comment) {
+      let res = await $axios.post("/api/comments", {
+        post_id: post.id,
+        comment: comment,
+      });
+
+      if (res.status === 200) {
+        await this.updateComments(post);
+      }
+    },
+
+    async deleteComment(post, commentId) {
+      let res = await $axios.delete(`/api/comments/${commentId}`, {
+        post_id: post.id,
+      });
+
+      if (res.status === 200) {
+        await this.updateComments(post);
+      }
+    },
+
+    async updateComments(post) {
+      let res = await $axios.get(`/api/profiles/${post.user.id}`);
+
+      for (let i = 0; i < res.data.posts.length; i++) {
+        const updatePost = res.data.posts[i];
+
+        if (post.id == updatePost.id) {
+          useGeneralStore().selectedPost.comments = updatePost.comments;
+        }
+      }
+    },
+
+    async likePost(post, isPostPage) {
+      let res = await $axios.post("/api/likes", {
+        post_id: post.id,
+      });
+
+      console.log(res);
+
+      let singlePost = null;
+
+      if (isPostPage) {
+        singlePost = post;
+      } else {
+        singlePost = useGeneralStore().posts.find((p) => p.id === post.id);
+      }
+      console.log(singlePost);
+      singlePost.likes.push(res.data.like);
+    },
+
+    async unlikePost(post, isPostPage) {
+      let deleteLike = null;
+      let singlePost = null;
+
+      if (isPostPage) {
+        singlePost = post;
+      } else {
+        singlePost = useGeneralStore().posts.find((p) => p.id === post.id);
+      }
+
+      singlePost.likes.forEach((like) => {
+        if (like.user_id === this.id) {
+          deleteLike = like;
+        }
+      });
+
+      let res = await $axios.delete("/api/likes/" + deleteLike.id);
+
+      for (let i = 0; i < singlePost.likes.length; i++) {
+        const like = singlePost.likes[i];
+        if (like.id === res.data.like.id) {
+          singlePost.likes.splice(i, 1);
+        }
+      }
+    },
+
     async logout() {
       await $axios.post("/logout");
       this.resetUser();
@@ -57,15 +148,6 @@ export const useUserStore = defineStore("user", {
       this.$state.email = "";
       this.$state.bio = "";
       this.$state.image = "";
-    },
-
-    async getUser() {
-      let res = await $axios.get("/api/logged-in-user");
-
-      this.$state.id = res.data[0].id;
-      this.$state.name = res.data[0].name;
-      this.$state.bio = res.data[0].bio;
-      this.$state.image = res.data[0].image;
     },
   },
   persist: true,
